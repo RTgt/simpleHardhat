6f382835c3247f8acbb98fd3115ae3f5b017d412
RT <artiom04@mail.ru>
2022-02-04 11:47:34 -0800

    struct Donator, array donators added, pragma abicoder v2 added, now the contract stores and returns the array of structs

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,11 +1,17 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+pragma abicoder v2;
 
 import "hardhat/console.sol";
 
 contract Donation {
     address payable public owner;
-    address[] public donators;
+    
+    struct Donator {
+        address adress;
+        uint value;
+    }
+    Donator[] public donators;
 
     constructor() {
         owner = payable (msg.sender);
@@ -13,13 +19,15 @@ contract Donation {
 
     function gatherDonation() public payable {
         require(msg.value >= .001 ether);
-        donators.push(msg.sender);
+        donators.push(Donator(owner, msg.value));
     }
+
     function transferToOwner() external {
         require(msg.sender == owner);
         owner.transfer(address(this).balance);
     }
-    function getDonators() public view returns(address[] memory){
+
+    function getDonators() public view returns(Donator[] memory){
         return donators;
     }
     
